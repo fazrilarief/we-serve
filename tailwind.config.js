32727c93ac4b46310b288ae99d5a80df5df1bce8
Fazril Arief Nugraha <fazrilarief331@gmail.com>
2024-04-10 14:59:53 +0700

    first commit and create navbar

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4f46e5",
+        secondary: "#080C2E",
+      },
+    },
+  },
+  plugins: [],
+};
